e4373a9b9f435808a93cc53500aaaa394b283362
Hilal Yazbek <hilal_yazbeck@hotmail.com>
2023-05-31 18:48:18 +0300

    implemented *ngOnChanges

--- hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,17 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent {
-
+export class RoomsListComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
   @Output() roomSelected = new EventEmitter<RoomList>();
   @Input() rooms: RoomList[] = [];
 
-  selectRoom(room: RoomList){
+  selectRoom(room: RoomList) {
     this.roomSelected.emit(room);
   }
 }
--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -7,56 +7,77 @@ import { Room, RoomList } from './rooms';
   styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
-
   ngOnInit(): void {
-    this.roomList = [{
-      roomNumber: 111,
-      roomType : 'Delux Room',
-      amenities: 'Air Conditioner, wifi, tv, bathroom',
-      price: 500,
-      photos: 'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
-      checkinTime : new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021')
-    }, {
-      roomNumber: 211,
-      roomType : 'Regular Room',
-      amenities: 'Air Conditioner, bathroom',
-      price: 200,
-      photos: 'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
-      checkinTime : new Date('13-Nov-2021'),
-      checkoutTime: new Date('16-Nov-2021')
-    }, {
-      roomNumber: 311,
-      roomType : 'Mini Room',
-      amenities: 'Air Conditioner',
-      price: 100,
-      photos: 'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
-      checkinTime : new Date('11-Nov-2021'),
-      checkoutTime: new Date('21-Nov-2021')
-    },
-  ];
+    this.roomList = [
+      {
+        roomNumber: 111,
+        roomType: 'Delux Room',
+        amenities: 'Air Conditioner, wifi, tv, bathroom',
+        price: 500,
+        photos:
+          'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+      },
+      {
+        roomNumber: 211,
+        roomType: 'Regular Room',
+        amenities: 'Air Conditioner, bathroom',
+        price: 200,
+        photos:
+          'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
+        checkinTime: new Date('13-Nov-2021'),
+        checkoutTime: new Date('16-Nov-2021'),
+      },
+      {
+        roomNumber: 311,
+        roomType: 'Mini Room',
+        amenities: 'Air Conditioner',
+        price: 100,
+        photos:
+          'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('21-Nov-2021'),
+      },
+    ];
   }
-  constructor(){
+  constructor() {
     console.log('constructor');
   }
   hotelName = 'Hilton Hotel';
   numberOfRooms = 100;
 
   rooms: Room = {
-    availableRooms : 5,
-    bookedRooms : 5,
-    totalRooms : 20,
+    availableRooms: 5,
+    bookedRooms: 5,
+    totalRooms: 20,
   };
 
-  roomList: RoomList [] = []
+  roomList: RoomList[] = [];
 
   hideRooms = false;
   toggle() {
     this.hideRooms = !this.hideRooms;
   }
 
-  selectedRoom! : RoomList;
-  selectRoom(room: RoomList){
+  selectedRoom!: RoomList;
+  selectRoom(room: RoomList) {
     this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 321,
+      roomType: 'penthouse',
+      amenities: '2 bathrooms',
+      price: 2000,
+      photos:
+        'https://www.usatoday.com/gcdn/-mm-/05b227ad5b8ad4e9dcb53af4f31d7fbdb7fa901b/c=0-64-2119-1259/local/-/media/USATODAY/USATODAY/2014/08/13/1407953244000-177513283.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp',
+      checkinTime: new Date('1-jan-2022'),
+      checkoutTime: new Date('10-jan-2023'),
+    };
+
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
   }
 }
