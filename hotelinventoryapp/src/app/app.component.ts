59d2a55854853d89f8b68474242ae2174c4a464c
Hilal Yazbek <hyazbek@psa.gov.qa>
2023-06-01 09:49:17 +0300

    content projection ordering elements in containers

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
-
-  role = 'Admin';
 }
